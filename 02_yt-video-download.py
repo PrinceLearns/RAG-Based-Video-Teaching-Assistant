31cce13dbde4d558778aaac9b8211871875fd8f1
Prince Parmar <princeparmar2209@gmail.com>
2026-02-03 21:28:10 +0530

    adding gitignore file

--- 02_yt-video-download.py
+++ 02_yt-video-download.py
@@ -3,7 +3,7 @@
 
 import yt_dlp
 
-url = "https://www.youtube.com/watch?v=tVzUXW6siu0&list=PLu0W_9lII9agq5TrH9XLIKQvv0iaF2X3w&index=1"
+url = "link for videos"
 
 ydl_opts = {
     'format': 'best',
